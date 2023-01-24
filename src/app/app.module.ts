31e301c738ad7662f6d60945a0dcca7b984250b1
PC <fernandezfrancoa2001@gmail.com>
2023-01-24 17:56:24 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
+import { HeaderComponent } from './shared/header/header.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonListComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pokemon/pokemon.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
+
+
+
+@NgModule({
+  declarations: [
+    PokemonListComponent,
+    PokemonDetailsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PokemonModule { }
